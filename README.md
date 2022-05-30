1c6797c80c2a0ff484744ac573f535072c1b8af0
Jenny <10570219+jen-w@users.noreply.github.com>
2022-05-29 17:56:31 -0700

    Add getting tweets from timeline option to go in from new to old
    Because why not

--- README.md
+++ README.md
@@ -1,19 +1,30 @@
 # tweeter-deleter
 
-This is a script I wrote to review and delete tweets sourced from my tweet archive one-by-one. Background is that I used https://github.com/koenrh/delete-tweets to bulk delete most of my (definitely filler, not killer) tweets but there is still a body of tweets remaining that I want to sift through manually cuz I'm really funny.
+This is a script I wrote to review and delete tweets sourced from my tweet archive one-by-one. It can also delete recent tweets pulled from the API. Background is that I used https://github.com/koenrh/delete-tweets to bulk delete most of my (definitely filler, not killer) tweets but there is still a body of tweets remaining that I want to sift through manually cuz I'm really funny.
 
 ## Usage
-### First time
-1. Create a Twitter dev account, create an app with read/write permissions, and get credentials for your own account. Add the following env vars: `TWITTER_ACCESS_TOKEN`, `TWITTER_ACCESS_TOKEN_SECRET`, `TWITTER_CONSUMER_KEY`, `TWITTER_CONSUMER_SECRET` to your environment. If you can't be bothered to do this, that's fine. You can just open the link and delete manually.
-2. Execute `npm run start ~/path/to/twitter-2022-blah/data/tweet.js`
-3. A tweet and some details of it will be printed and the tool will prompt you to either: `o to open, d to delete, x to exit`. Just hit enter if you want to keep.  
+
+### From API
+1. Create a Twitter dev account, create an app with read/write permissions, and get credentials for your own account. Add the following env vars: `TWITTER_ACCESS_TOKEN`, `TWITTER_ACCESS_TOKEN_SECRET`, `TWITTER_CONSUMER_KEY`, `TWITTER_CONSUMER_SECRET` to your environment.
+2. Execute `npm run start`
+3. A tweet and some details of it will be printed and the tool will prompt you to either: `o to open, k to keep, x to exit`. Just hit enter if you want to delete.  
 o = open link to tweet in browser  
-d = delete it  
+k = keep it  
+x = exit script
+
+### From archive
+
+#### First time
+1. Create a Twitter dev account, create an app with read/write permissions, and get credentials for your own account. Add the following env vars: `TWITTER_ACCESS_TOKEN`, `TWITTER_ACCESS_TOKEN_SECRET`, `TWITTER_CONSUMER_KEY`, `TWITTER_CONSUMER_SECRET` to your environment. If you can't be bothered to do this, that's fine. You'll need to do some hardcoding/commenting out in the script. You can just open the link and delete manually.
+2. Execute `npm run start ~/path/to/twitter-2022-blah/data/tweet.js`
+3. A tweet and some details of it will be printed and the tool will prompt you to either: `o to open, k to keep, x to exit`. Just hit enter if you want to delete.  
+o = open link to tweet in browser  
+k = keep it  
 x = exit script
 4. Upon exiting, a "pagination" token (it's the id of the tweet you left off on) will be printed. Copy this for next time.
 
-### Next time
+#### Next time
 Execute `npm run start ~/path/to/twitter-2022-blah/data/tweet.js ["pagination" token]`
 
-### Recommendation
+#### Recommendation
 Delete your rt's and bunk tweets using the aforementioned delete-tweets command line tool. Then, request your tweet archive again and use this script (if you so wish to).
--- index.js
+++ index.js
@@ -3,39 +3,54 @@ const prompt = require('prompt-sync')();
 const tw = require('twitter-api-v2');
 const open = require('open');
 
-// first arg: file path to tweet.js
-const data = fs.readFileSync(process.argv[2], 'utf8');
-const jsonString = data.replace(/window.YTD.tweet.part0 = /g, '');
-const tweets = JSON.parse(jsonString);
+const client = new tw.TwitterApi({
+    appKey: process.env.TWITTER_CONSUMER_KEY,
+    appSecret: process.env.TWITTER_CONSUMER_SECRET,
+    accessToken: process.env.TWITTER_ACCESS_TOKEN,
+    accessSecret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
+});
 
-console.log(`Total in tweet.js: ${tweets.length} tweets!`);
-
-// second arg: pagination token / last id looked at
-const pageToken = process.argv[3];
+let tweets = null;
 let i = 0;
 
-if (pageToken != null && pageToken !== "") {
-    let target = tweets[i];
-    while (target.tweet.id_str !== pageToken) {
-        i++;
-        target = tweets[i];
-    }
+const readArchive = (path) => {
+    const data = fs.readFileSync(path, 'utf8');
+    const jsonString = data.replace(/window.YTD.tweet.part0 = /g, '');
+    tweets = JSON.parse(jsonString);
 
+    console.log(`Total in tweet.js: ${tweets.length} tweets!`);
+
+    // second arg: pagination token / last id looked at
+    const pageToken = process.argv[3];
+
+    if (pageToken != null && pageToken !== "") {
+        let target = tweets[i];
+        while (target.tweet.id_str !== pageToken) {
+            i++;
+            target = tweets[i];
+            console.log(target);
+        }
+    }
 }
 
-const main = async () => {
-    const client = new tw.TwitterApi({
-        appKey: process.env.TWITTER_CONSUMER_KEY,
-        appSecret: process.env.TWITTER_CONSUMER_SECRET,
-        accessToken: process.env.TWITTER_ACCESS_TOKEN,
-        accessSecret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
-    });
+const processTweet = async (id, link) => {    
+    const answer = prompt("What'll it be? (o to open, k to keep, x to exit, enter to delete) ");
+    if (answer === "k" || answer === ".") {
+        console.log("OK keeping.");
+    } else if (answer === "o") {
+        console.log("OK opening.");
+        open(link);
+    } else if (answer === "x") {
+        console.log(`OK. Pagination token for next time in case you need it: ${id}`);
+        process.exit();
+    } else {
+        console.log("OK deleting.");
+        const deletedTweet = await client.v1.deleteTweet(id);
+        console.log("Deleted tweet", deletedTweet.id_str, ":", deletedTweet.full_text);
+    }
+}
 
-    const resp = await client.v1.verifyCredentials();
-    const username = resp.screen_name;
-    console.log(`Just gonna assume this is u: @${username}`);
-    console.log(`Actual # tweets: ${resp.statuses_count}\n`);
-    
+const processTweetsFromArchive = async (username) => {    
     while (i < tweets.length) {
         const target = tweets[i];
         const relevant = {
@@ -46,23 +61,41 @@ const main = async () => {
             link: `https://twitter.com/${username}/status/${target.tweet.id_str}`
         };
         console.log(relevant);
-    
-        const answer = prompt("What'll it be? (o to open, d to delete, x to exit) ");
-        if (answer === "d") {
-            console.log("OK deleting.");
-            const deletedTweet = await client.v1.deleteTweet(target.tweet.id_str);
-            console.log('Deleted tweet', deletedTweet.id_str, ':', deletedTweet.full_text);
-        } else if (answer === "o") {
-            console.log("OK opening.");
-            open(relevant.link);
-        } else if (answer === "^C" || answer === "x") {
-            console.log(`OK. Pagination token for next time: ${target.tweet.id_str}`);
-            return;
-        } else {
-            console.log("OK.");
-        }
+        await processTweet(target.tweet.id_str, relevant.link);
     
         i++;
+    }
+}
+
+const processTweetsFromApi = async (username) => {    
+    const userTimeline = await client.v1.userTimelineByUsername(username);
+    for await (const tweet of userTimeline) {
+        const relevant = {
+            retweet_count: tweet.retweet_count,
+            favorite_count: tweet.favorite_count,
+            created_at: tweet.created_at,
+            full_text: tweet.full_text,
+            link: `https://twitter.com/${username}/status/${tweet.id_str}`
+        };
+        console.log(relevant);
+        await processTweet(tweet.id_str, relevant.link);
+    }
+}
+
+const main = async () => {
+    const resp = await client.v1.verifyCredentials();
+    const username = resp.screen_name;
+    console.log(`Just gonna assume this is u: @${username}`);
+    console.log(`Actual # tweets: ${resp.statuses_count}\n`);
+
+    // first arg: file path to tweet.js. if null, pull tweets from api.
+    const firstArg = process.argv[2];
+
+    if (firstArg && firstArg !== "") {
+        readArchive(firstArg);
+        await processTweetsFromArchive(username);
+    } else {
+        await processTweetsFromApi(username);
     }
 }
 
